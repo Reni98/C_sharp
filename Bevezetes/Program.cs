2276e88c4c4038f54ce09ee862364f4a4225daa5
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-01-19 09:18:25 +0100

    Add files via upload

---
+++ Bevezetes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bevezetés
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Bevezetés a C# nyelvbe
+            1.Mi az a C#?
+            C# (ejtsd: "C sharp") egy objektumorientált programozási nyelv, amelyet a Microsoft fejlesztett ki a 
+            .NET keretrendszer számára. 
+            A nyelvet Anders Hejlsberg vezette csapat alkotta meg, és először 2000-ben mutatták be. 
+            A C# erős típusellenőrzést, automatikus memóriakezelést, és modern nyelvi konstrukciókat kínál,
+            ami egyszerre teszi biztonságossá és hatékonnyá a fejlesztést.
+
+            2. Történeti háttér
+            A C# nyelv megalkotásának célja egy olyan fejlesztési eszköz létrehozása volt,
+            amely megfelel a modern alkalmazásfejlesztési igényeknek, különösen a Windows platformon.
+            A nyelv azóta széles körben elterjedt, és különböző alkalmazások fejlesztésére használják, 
+            beleértve a web-, asztali-, mobil-, és játékfejlesztést is.
+
+            3. A .NET keretrendszer
+            A C# szorosan integrálódik a .NET keretrendszerrel, amely egy szoftverfejlesztési platform. 
+            A .NET számos szolgáltatást nyújt, mint például a:
+
+            BCL (Base Class Library): Alapvető osztálykönyvtárak, amelyek széleskörű funkciókat biztosítanak, 
+            mint például adatbázis-kezelés, fájlkezelés, hálózati kommunikáció, stb.
+            CLR (Common Language Runtime): Egy futtatókörnyezet, amely biztosítja a programok futtatását és
+            menedzselt kódvégrehajtást, beleértve a szemétgyűjtést (garbage collection), kivételkezelést, 
+            és típusbiztonságot.
+            ASP.NET: Egy keretrendszer webes alkalmazások és szolgáltatások fejlesztéséhez.
+            Xamarin: Egy eszközkészlet mobilalkalmazások fejlesztéséhez.
+
+            4. A C# szintaxisa
+            A C# szintaxisa sokban hasonlít más C-alapú nyelvekhez, mint például a C++, Java, és JavaScript.
+            Ez megkönnyíti a C# megtanulását azok számára, akik már ismernek más programozási nyelveket.
+
+            Példa: Egy egyszerű "Hello, World!" program C#-ban           
+             */
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadKey();
+        }
+    }
+}
