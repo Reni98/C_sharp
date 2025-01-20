9b0b5e3249e632e10d3e9a9f55f499a09170420b
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-01-20 20:36:52 +0100

    Add files via upload

---
+++ SwitchCase/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Bevezetés a switch-case szerkezetbe
+                A switch-case egy döntési szerkezet, amely több ágra oszthatja a program vezérlését 
+                az alapján, hogy egy kifejezés értéke milyen. Különösen hasznos akkor, ha több if-else ágat kellene egymás után írnunk, mert sokkal olvashatóbb és hatékonyabb megoldást nyújt.
+
+            Általános szintaxis
+            switch (kifejezés)
+{
+                case érték1:
+                 // Kód, ha a kifejezés értéke érték1
+                     break;
+
+                case érték2:
+                // Kód, ha a kifejezés értéke érték2
+                    break;
+
+                default:
+                // Kód, ha egyik érték sem egyezik
+                    break;
+
+            Fontos részek
+            switch (kifejezés): Az a változó vagy kifejezés, amit ellenőrizni szeretnénk.
+            case érték:: Egy lehetséges érték, amire a kifejezés egyezhet.
+            break: Megszakítja a switch blokk végrehajtását, különben a következő ágra is átlépne (ez az ún. "fall-through" viselkedés).
+            default:: Opcionális ág, ami akkor fut, ha egyik case sem teljesül.
+}
+
+             */
+
+            Console.WriteLine("Válassz egy opciót:");
+            Console.WriteLine("1 - Kávé");
+            Console.WriteLine("2 - Tea");
+            Console.WriteLine("3 - Víz");
+            Console.Write("Írd be a választott számot: ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Kávét választottál.");
+                    break;
+                case 2:
+                    Console.WriteLine("Teát választottál.");
+                    break;
+                case 3:
+                    Console.WriteLine("Vizet választottál.");
+                    break;
+                default:
+                    Console.WriteLine("Érvénytelen választás.");
+                    break;
+            }
+
+
+            /*
+             * Mikor érdemes használni a switch-case-t?
+                1. Több lehetséges érték ellenőrzésekor
+                    Ha egy változó több különböző értéket vehet fel, és mindegyikhez más-más művelet tartozik, akkor a switch-case tisztább és olvashatóbb megoldás.
+
+                    Miért jobb ez, mint az if-else?
+
+                    Az if-else struktúra nehezen olvashatóvá válik, ha sok ágat kell kezelni.
+                    A switch-case esetén az egyes ágak elkülönülnek, így könnyebben karbantartható a kód.
+
+                2. Egymáshoz hasonló logika esetén
+                    Ha az egyes esetek ugyanazt a változót vizsgálják és hasonló műveleteket hajtanak végre, a switch-case egyszerűsíti a kódot.
+
+                3. Rövid, jól elkülöníthető esetekhez
+                A switch-case jól működik, ha az egyes ágak kódja rövid és egyszerű. Ha bonyolult logikát kell kezelni, érdemes inkább függvényeket hívni a case ágakban.
+
+                4. Számok és szövegek kezelésére
+                A switch-case támogatja az egész számokat, a szövegeket és a logikai értékeket is (C# 7.0 óta). Ha a feltétel logikai értéken vagy egy adott számításon alapul, szintén jó választás.
+
+
+                ### **Összefoglalva**
+                Használj **`switch-case`-t**, ha:
+                - Egy változó több lehetséges értékét kell kezelni.
+                - Az értékek egymástól függetlenek.
+                - A kód olvashatóságát és karbantarthatóságát szeretnéd javítani.
+
+                Használj **`if-else`-t**, ha:
+                - Komplex feltételekkel kell dolgozni.
+                - Az ellenőrzés nem determinisztikus vagy dinamikus.
+             */
+        }
+    }
+}
---
+++ While_and_Doe_while/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_and_Do_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * While és Do-While ciklusok 
+             * A while és do-while ciklusok olyan vezérlési szerkezetek, amelyek lehetővé teszik, hogy egy kódrészletet addig ismételj, amíg egy feltétel igaz. 
+             * Ezek különböznek a for ciklustól abban, hogy inkább akkor használjuk őket, ha nem tudjuk előre, hogy hányszor kell végrehajtani a ciklust.
+
+            1. A while ciklus
+                A while ciklus az ismétlés előtt ellenőrzi a feltételt, és csak akkor hajtja végre a ciklusmagot, ha a feltétel igaz.
+
+                Szintaxis:
+
+                while (feltétel)
+                  {
+                    // Kód, ami ismétlődik, ha a feltétel igaz
+                   }   
+   
+
+                Példa:
+             Addig kérünk be számokat a felhasználótól, amíg nullát nem ír be:
+             */
+
+            Console.WriteLine("Írj be számokat! A ciklus nullával ér véget.");
+            int number = int.Parse(Console.ReadLine());
+
+            while (number != 0)
+            {
+                Console.WriteLine($"A beírt szám: {number}");
+                number = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("A ciklus véget ért.");
+
+
+
+            /*
+             * 2. A do-while ciklus
+            A do-while ciklus legalább egyszer végrehajtja a ciklusmagot, mivel a feltételt csak az ismétlés után ellenőrzi.
+
+            Szintaxis:
+            do
+               {
+                // Kód, ami legalább egyszer végrehajtódik
+                }while (feltétel);
+
+             Példa:
+            A program legalább egyszer bekéri a felhasználó nevét, még akkor is, ha az első próbálkozás sikertelen:
+             */
+
+            string name;
+
+            do
+            {
+                Console.Write("Írd be a neved (legalább 3 karakter): ");
+                name = Console.ReadLine();
+            } while (name.Length < 3);
+
+            Console.WriteLine($"Üdv, {name}!");
+
+
+            /*
+             * 3. Fő különbségek a while és a do-while között
+                Tulajdonság	            while	                                    do-while
+                Feltétel ellenőrzése	Az ismétlés előtt	                        Az ismétlés után
+                Legalább egyszer fut?	Nem garantált	                            Legalább egyszer végrehajtódik
+                Használat	            Ha a feltétel már kezdetben hamis	        Ha a ciklust legalább egyszer végre kell hajtani
+
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
