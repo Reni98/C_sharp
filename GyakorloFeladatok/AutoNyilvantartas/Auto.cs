f7928af5e9abae929d9034490fab1d3fb59cbfee
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-03 11:54:00 +0100

    Update Auto.cs

--- GyakorloFeladatok/AutoNyilvantartas/Auto.cs
+++ GyakorloFeladatok/AutoNyilvantartas/Auto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +8,39 @@ namespace Cars
 {
     internal class Auto
     {
+        
+        public int id;
+        public string marka;
+        public string rendszam;
+        public int ar;
+        public int db;
+
+        public Auto(string id, string marka, string rendszam, string ar, string db)
+        {
+            this.id = int.Parse(id);
+            this.marka = marka;
+            this.rendszam = rendszam;
+            this.ar = int.Parse(ar);
+            this.db = int.Parse(db);
+        }
+
+        public static int Megszamol(List<Auto> autolista) { 
+            return autolista.Count();
+        }
+
+        public static int Dragabb(List<Auto> autolista, int d) {
+            int dragabb = 0;
+            foreach (var item in autolista)
+            {
+                if (item.ar > d)
+                {
+                    dragabb++;
+                }
+                else {
+                    Console.WriteLine("Érvénytelen ár.");
+                }
+            }
+            return dragabb;
+        }
     }
 }
