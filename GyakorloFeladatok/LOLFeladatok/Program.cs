719c30e0737488035b51e202c9110d2482ce3efc
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-04 13:28:48 +0100

    Update Program.cs

--- GyakorloFeladatok/LOLFeladatok/Program.cs
+++ GyakorloFeladatok/LOLFeladatok/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
+using static Org.BouncyCastle.Asn1.Cmp.Challenge;
 
 namespace LOL
 {
@@ -24,22 +25,41 @@ namespace LOL
                 jatekok.Add(ujjatek);
             }
 
-            foreach (var item in jatekok)
-            {
-                Console.WriteLine($"{item.sorszam},{item.rang},{item.meccsek},{item.veresegek},{item.karakterek},{item.pozicio},{item.felhnev}");
-            }
+            
             using (MySqlConnection conn = new MySqlConnection(connectionString))
                 try
                 {
                 conn.Open();
+                    /*
                 Console.WriteLine("Sikeres kapcsolat az adatbázishoz!");
+                    Console.WriteLine("Melyik soszámú játékot szeretnéd frissiteni?");
+                    Console.WriteLine("Add meg a sorszámot: ");
+                    int sorszam = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Add meg a rangot:");
+                    string rang = Console.ReadLine();
+                    Console.WriteLine("Add meg a meccsek számát");
+                    int meccsek = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Add meg a vereségek számát");
+                    int veresegek = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Add meg a karaktert");
+                    string karakterek = Console.ReadLine();
+                    Console.WriteLine("Add meg poziciókat:");
+                    string pozicio = Console.ReadLine();
+                    Console.WriteLine("Add meg a felhasználónevet");
+                    string felhnev = Console.ReadLine();
+                    //UjJatek(sorszam,rang,meccsek,veresegek,karakterek,pozicio,felhnev);
+                    JatekFrissitese(sorszam, rang, meccsek, veresegek, karakterek, pozicio, felhnev);
+                    */
 
-                //UjFelhasznalo("Kiss Péter", "peter@example.com");
-                //UjFelhasznalo("Nagy Anna", "anna@example.com");
+                    int felhszama = LOL.JatekosMegszamol(jatekok);
+                    Console.WriteLine($"A txt fájlban {felhszama} felhasználó van.");
 
+                    List <string>nevek= LOL.NevekMegszamol(jatekok);
+                    Console.WriteLine(string.Join(",",nevek));
+                    Console.WriteLine($"S betűvel kezdődő nevek db száma: {nevek.Count}");
 
-            }
-            catch (Exception ex)
+                }
+                catch (Exception ex)
             {
                 Console.WriteLine("Hiba a kapcsolat során: " + ex.Message);
             }
@@ -57,6 +77,32 @@ namespace LOL
                 {
                     cmd.Parameters.AddWithValue("@sorszam", sorszam);
                     cmd.Parameters.AddWithValue("@rang", rang);
+                    cmd.Parameters.AddWithValue("@meccsek", meccsek);
+                    cmd.Parameters.AddWithValue("@veresegek", veresegek);
+                    cmd.Parameters.AddWithValue("@karakterek", karakterek);
+                    cmd.Parameters.AddWithValue("@pozicio", pozicio);
+                    cmd.Parameters.AddWithValue("@felhnev", felhnev);
+
+
+
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        static void JatekFrissitese(int sorszam, string rang, int meccsek, int veresegek, string karakterek, string pozicio, string felhnev)
+        {
+            string query = "UPDATE meccsek SET rang = @rang, meccsek = @meccsek, veresegek = @veresegek, karakterek = @karakterek,pozicio = @pozicio,felhnev = @felhnev WHERE sorszam = @sorszam";
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@sorszam", sorszam);
+                    cmd.Parameters.AddWithValue("@rang", rang);
+                    cmd.Parameters.AddWithValue("@meccsek", meccsek);
                     cmd.Parameters.AddWithValue("@veresegek", veresegek);
                     cmd.Parameters.AddWithValue("@karakterek", karakterek);
                     cmd.Parameters.AddWithValue("@pozicio", pozicio);
