9aa197787fa3ff91f8105a0f7f3e61dd2ecd8ad9
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-10 11:13:00 +0100

    Add files via upload

---
+++ GyakorloFeladatok/Regisztracio/Program.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Bcpg;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regisztracio
+{
+    internal class Program
+    {
+        public static string connection = "server=localhost;database = regisztracio;user=root;password=;";
+        static void Main(string[] args)
+        {
+            List<Regisztracio> felhasznalok = new List<Regisztracio>();
+            string[] sorok = File.ReadAllLines("register.txt");
+            foreach (string sor in sorok) {
+                string[] adatok = sor.Split(';');
+                Regisztracio ujfelhasznalo = new Regisztracio(adatok[0],adatok[1],adatok[2],adatok[3],adatok[4]);
+                felhasznalok.Add(ujfelhasznalo);
+            }
+
+            foreach (var felh in felhasznalok) {
+                Console.WriteLine($"{felh.nev},{felh.felhnev},{felh.email}, {felh.jelszo}, {felh.szuldatum}");
+            }
+
+          
+                using (MySqlConnection conn = new MySqlConnection(connection)) {
+                try
+                {
+                    conn.Open();
+                    Console.WriteLine("Az adatbázis kapcsolat sikeresen létrejött!");
+                }
+                catch (Exception ex) {
+                    Console.WriteLine("Hiba történ a kapcsolódás során" + ex.Message);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        /*static void FelhasznaloHozzaad(string nev,string felhnev,string email,string jelszo,DateTime szuldatum) { 
+           
+        }*/
+
+
+    }
+}
---
+++ GyakorloFeladatok/Regisztracio/Regisztracio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regisztracio
+{
+    internal class Regisztracio
+    {
+        public string nev;
+        public string felhnev;
+        public string email;
+        public string jelszo;
+        public DateTime szuldatum;
+
+        public Regisztracio(string nev, string felhnev, string email, string jelszo, string szuldatum)
+        {
+            this.nev = nev;
+            this.felhnev = felhnev;
+            this.email = email;
+            this.jelszo = jelszo;
+            this.szuldatum = Convert.ToDateTime(szuldatum);
+        }
+    }
+}
