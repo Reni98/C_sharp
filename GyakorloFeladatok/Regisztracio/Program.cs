836041e8ecbf595591b81e5e31c5cf6323101dff
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-10 12:43:12 +0100

    Update Program.cs

--- GyakorloFeladatok/Regisztracio/Program.cs
+++ GyakorloFeladatok/Regisztracio/Program.cs
@@ -1,4 +1,4 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 using Org.BouncyCastle.Bcpg;
 using System;
 using System.Collections.Generic;
@@ -22,9 +22,7 @@ namespace Regisztracio
                 felhasznalok.Add(ujfelhasznalo);
             }
 
-            foreach (var felh in felhasznalok) {
-                Console.WriteLine($"{felh.nev},{felh.felhnev},{felh.email}, {felh.jelszo}, {felh.szuldatum}");
-            }
+            
 
           
                 using (MySqlConnection conn = new MySqlConnection(connection)) {
@@ -32,6 +30,15 @@ namespace Regisztracio
                 {
                     conn.Open();
                     Console.WriteLine("Az adatbázis kapcsolat sikeresen létrejött!");
+                    /*foreach (var felh in felhasznalok)
+                    {
+                        FelhasznaloHozzaad(felh.nev,felh.felhnev,felh.email, felh.jelszo, felh.szuldatum);
+
+                    }
+                    Console.WriteLine("Sikeresen fel lettek töltve az adatok az adatbázisba!");
+                    */
+
+                    FelhasznaloListazas();
                 }
                 catch (Exception ex) {
                     Console.WriteLine("Hiba történ a kapcsolódás során" + ex.Message);
@@ -40,9 +47,47 @@ namespace Regisztracio
             Console.ReadKey();
         }
 
-        /*static void FelhasznaloHozzaad(string nev,string felhnev,string email,string jelszo,DateTime szuldatum) { 
-           
-        }*/
+        static void FelhasznaloHozzaad(string nev,string felhnev,string email,string jelszo,DateTime szuldatum) {
+            string query = "INSERT INTO userek (nev,felhnev,email,jelszo,szuldatum) VALUES (@nev,@felhnev,@email,@jelszo,@szuldatum)";
+            using (MySqlConnection conn = new MySqlConnection(connection)) {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                    cmd.Parameters.AddWithValue("@nev",nev);
+                    cmd.Parameters.AddWithValue("@felhnev",felhnev);
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@jelszo", jelszo);
+                    cmd.Parameters.AddWithValue("@szuldatum", szuldatum);
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+        }
+
+        static void FelhasznaloListazas() {
+            string query = "SELECT * FROM userek";
+
+            using (MySqlConnection conn = new MySqlConnection(connection)) {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query,conn)) {
+                    using (MySqlDataReader reader = cmd.ExecuteReader()) {
+                        while (reader.Read())
+                        {
+                         int id = reader.GetInt32("id");
+                        string nev = reader.GetString("nev");
+                        string felhnev = reader.GetString("felhnev");
+                        string email = reader.GetString("email");
+                        string jelszo = reader.GetString("jelszo");
+                        DateTime szuldatum = reader.GetDateTime("szuldatum");
+
+
+                      
+                         Console.WriteLine($"ID: {reader["id"]}, Nev: {reader["nev"]}, Felhasználónév: {reader["felhnev"]},Email: {reader["email"]}, Jelszó: {reader["jelszo"]}, Születési dátum: {reader["szuldatum"]}");
+                        }
+                    }
+                }
+
+            }
+        }
 
 
     }
