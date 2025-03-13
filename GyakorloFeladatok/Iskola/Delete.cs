c45ff3ae14ffcabb21ba6454d669856ff1aea6ad
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-13 09:17:20 +0100

    Add files via upload

---
+++ GyakorloFeladatok/Iskola/Delete.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suli
+{
+
+    internal class Delete
+    {
+        public static string connection = "server=localhost;database=iskola;user=root;password=;";
+
+        public static void AdatTorol(int diak_id) {
+            string query = "DELETE FROM diak WHERE diak_id = @diak_id";
+            using (MySqlConnection conn = new MySqlConnection(connection)) {
+                conn.Open();
+                try
+                {
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                        cmd.Parameters.AddWithValue("@diak_id",diak_id);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex) {
+
+                    Console.WriteLine("Hiba történt" + ex.Message);
+                }
+            
+            }
+
+        }
+    }
+}
---
+++ GyakorloFeladatok/Iskola/Update.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suli
+{
+    internal class Update
+    {
+        public static string connection = "server=localhost;database=iskola;user=root;password=;";
+
+        public static void AdatModosit(string regidveznev, string ujdveznev,string dkernev,string osztaly) {
+            string query = "UPDATE diak SET  dveznev = @ujdveznev,dkernev = @dkernev, osztaly = @osztaly WHERE dveznev=@regidveznev";
+            using (MySqlConnection conn = new MySqlConnection(connection)) {
+                conn.Open();
+                try {
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                        cmd.Parameters.AddWithValue("@regidveznev", regidveznev);
+                        cmd.Parameters.AddWithValue("@ujdveznev", ujdveznev);
+                        cmd.Parameters.AddWithValue("@dkernev", dkernev);
+                        cmd.Parameters.AddWithValue("@osztaly", osztaly);
+                        cmd.ExecuteNonQuery();
+                    }
+
+                } catch (Exception ex) {
+                    Console.WriteLine("Hiba történt" + ex.Message);
+                }
+            }
+        }
+    }
+}
