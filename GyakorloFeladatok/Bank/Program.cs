3b979f99ec24bf0f9b6fa7ddc66f2690428072f5
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-17 13:05:27 +0100

    Add files via upload

---
+++ GyakorloFeladatok/Bank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kérjetek be nevet, azonositot(8) és pinkodot(4)
+            SzamlaTulajdonos tulajdonos = new SzamlaTulajdonos("",0, 0);
+            var (tulajdonos_nev, azonosito, pinkod) = tulajdonos.TulajdonosAdatai();
+            Console.WriteLine($"A tulajdonos: {tulajdonos_nev}, azonositó: {azonosito}, pinkód: {pinkod}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GyakorloFeladatok/Bank/SzamlaTulajdonos.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class SzamlaTulajdonos
+    {
+        public string tulajdonos_nev;
+        public int azonosito;
+        public int pinkod;
+
+        public SzamlaTulajdonos(string tulajdonos_nev, int azonosito, int pinkod)
+        {
+            this.tulajdonos_nev = tulajdonos_nev;
+            this.azonosito = azonosito;
+            this.pinkod = pinkod;
+        }
+
+
+        public (string, int, int) TulajdonosAdatai()
+        {
+
+
+            int azonosito = 0;
+            int pinkod= 0;
+
+            while (azonosito == 0 && pinkod == 0) {
+                Console.WriteLine("Adja meg a nevét: ");
+                string tulajdonos_nev = Console.ReadLine();
+                Console.WriteLine("Adja meg az azonositó számát: ");
+                string bekertazonosito = Console.ReadLine();
+                Console.WriteLine("Adja meg a pinkodot:");
+                string bekertpinkod = Console.ReadLine();
+                if (bekertazonosito.Length != 8)
+                {
+                    Console.WriteLine(" 8 karaktert adjon meg");
+                }
+                else {
+                    azonosito = Convert.ToInt32(bekertazonosito);
+                    Console.WriteLine(azonosito);
+                }
+                if (bekertpinkod.Length != 4)
+                {
+                    Console.WriteLine("4 karaktert adjon meg");
+                }
+                else {
+                    pinkod = Convert.ToInt32(bekertpinkod);
+                    Console.WriteLine(pinkod);
+
+                }
+
+
+            }
+
+
+            
+                return (tulajdonos_nev, azonosito, pinkod);
+
+           
+
+
+        }
+
+        }
+
+    }
