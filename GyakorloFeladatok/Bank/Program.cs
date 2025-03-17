c02cf91962867aded12c1c8f6b2bf392fafc2d51
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-17 13:27:15 +0100

    Update Program.cs

--- GyakorloFeladatok/Bank/Program.cs
+++ GyakorloFeladatok/Bank/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +13,8 @@ namespace Bank
             //Kérjetek be nevet, azonositot(8) és pinkodot(4)
             SzamlaTulajdonos tulajdonos = new SzamlaTulajdonos("",0, 0);
             var (tulajdonos_nev, azonosito, pinkod) = tulajdonos.TulajdonosAdatai();
-            Console.WriteLine($"A tulajdonos: {tulajdonos_nev}, azonositó: {azonosito}, pinkód: {pinkod}");
+            Create.TulajdonosAdatFeltolt(tulajdonos_nev,azonosito,pinkod);
+            Console.WriteLine("Sikeres volt az adatfeltöltés.");
             Console.ReadKey();
         }
     }
