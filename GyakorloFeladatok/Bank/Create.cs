23fd015ffce038243857911dbddd70e6bc04895e
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-17 13:27:35 +0100

    Add files via upload

---
+++ GyakorloFeladatok/Bank/Create.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Create
+    {
+        public static string connect = "server=localhost;database = bank;user = root; password = ;";
+
+
+        public static void TulajdonosAdatFeltolt(string tulajdonos_nev, int azonosito,int pinkod) {
+            string query = "INSERT INTO szamla_tulajdonosok (tulajdonos_nev,azonosito,pinkod) VALUES (@tulajdonos_nev,@azonosito,@pinkod)";
+            using (MySqlConnection conn = new MySqlConnection(connect)) {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query,conn)) {
+                    cmd.Parameters.AddWithValue("@tulajdonos_nev", tulajdonos_nev);
+                    cmd.Parameters.AddWithValue("@azonosito", azonosito);
+                    cmd.Parameters.AddWithValue("@pinkod",pinkod);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
