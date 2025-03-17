0925bbb06695554f76b01d888e5fa50c437f1b3d
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-17 11:28:59 +0100

    Update Program.cs

--- GyakorloFeladatok/Szojatek/Program.cs
+++ GyakorloFeladatok/Szojatek/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +10,12 @@ namespace Szojatek
     {
         static void Main(string[] args)
         {
-            Jatek.UjJatek();
+            Jatek jatekok = new Jatek("",0,0);
+            var (jatekos_nev, pont, jatek) = jatekok.UjJatek();
+
+            int jatekos_id = Create.JatekosAdatFeltolt(jatekos_nev);
+            Create.JatekAdatFeltolt(pont,jatek,jatekos_id);
+            Console.WriteLine("Az adatok sikeresen fellettek töltve az adatbázisba.");
 
             Console.ReadKey();
         }
