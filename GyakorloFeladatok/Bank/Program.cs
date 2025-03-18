b0a08a7e48ead415347988c945361fded7f0d5c5
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-18 13:00:02 +0100

    Update Program.cs

--- GyakorloFeladatok/Bank/Program.cs
+++ GyakorloFeladatok/Bank/Program.cs
@@ -10,11 +10,37 @@ namespace Bank
     {
         static void Main(string[] args)
         {
-            //Kérjetek be nevet, azonositot(8) és pinkodot(4)
-            SzamlaTulajdonos tulajdonos = new SzamlaTulajdonos("",0, 0);
-            var (tulajdonos_nev, azonosito, pinkod) = tulajdonos.TulajdonosAdatai();
-            Create.TulajdonosAdatFeltolt(tulajdonos_nev,azonosito,pinkod);
-            Console.WriteLine("Sikeres volt az adatfeltöltés.");
+            int azonositas = 0;
+            int pinkodmegad = 0;
+            string tulajdonos_neve = "";
+            
+
+            Console.WriteLine("Nálunk bankol már?");
+            string valasz = Console.ReadLine();
+
+            if (valasz == "nem")
+            {
+                SzamlaTulajdonos tulajdonos = new SzamlaTulajdonos("", 0, 0);
+                var (tulajdonos_nev, azonosito, pinkod) = tulajdonos.TulajdonosAdatai();
+                Create.TulajdonosAdatFeltolt(tulajdonos_nev, azonosito, pinkod);
+                Console.WriteLine("Sikeres volt az adatfeltöltés.");
+
+            }
+            else {
+                if (Ellenorzes.AdatEllenorzes(tulajdonos_neve, azonositas, pinkodmegad))
+                {
+                    Console.WriteLine("Sikeresen belépett a fiókjába.");
+                }
+                else
+                {
+                    Console.WriteLine("Hibás adatot adott meg.");
+                }
+            }
+
+
+           
+           
+         
             Console.ReadKey();
         }
     }
