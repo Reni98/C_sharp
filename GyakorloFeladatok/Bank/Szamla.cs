eeaed10f355110b284f104ed78cd80309ca77dcf
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-20 10:32:19 +0100

    Add files via upload

---
+++ GyakorloFeladatok/Bank/Szamla.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class Szamla
+    {
+        public (string,decimal) SzamlaLetrehozas() {
+            Console.WriteLine("Add meg a számlászámod:");
+            string szamlaszam = Console.ReadLine();
+
+            Console.WriteLine("Mekkora összeget tennél be:");
+            decimal egyenleg = decimal.Parse(Console.ReadLine());
+
+            return (szamlaszam, egyenleg);
+        }
+
+    }
+}
