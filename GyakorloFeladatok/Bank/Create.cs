d42c2d632c1142e8aecc936d1b98bce05154beb5
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-20 10:29:45 +0100

    Update Create.cs

--- GyakorloFeladatok/Bank/Create.cs
+++ GyakorloFeladatok/Bank/Create.cs
@@ -1,8 +1,10 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Bcpg.OpenPgp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +15,7 @@ namespace Bank
         public static string connect = "server=localhost;database = bank;user = root; password = ;";
 
 
-        public static void TulajdonosAdatFeltolt(string tulajdonos_nev, int azonosito,int pinkod) {
+        public static int TulajdonosAdatFeltolt(string tulajdonos_nev, int azonosito,int pinkod) {
             string query = "INSERT INTO szamla_tulajdonosok (tulajdonos_nev,azonosito,pinkod) VALUES (@tulajdonos_nev,@azonosito,@pinkod)";
             using (MySqlConnection conn = new MySqlConnection(connect)) {
                 conn.Open();
@@ -22,8 +24,30 @@ namespace Bank
                     cmd.Parameters.AddWithValue("@azonosito", azonosito);
                     cmd.Parameters.AddWithValue("@pinkod",pinkod);
                     cmd.ExecuteNonQuery();
+                    string tulajdonosid = "SELECT LAST_INSERT_ID()";
+
+                    using (MySqlCommand idcmd = new MySqlCommand(tulajdonosid, conn)) {
+                        int tulajdonos_id = Convert.ToInt32(idcmd.ExecuteScalar());
+                        return tulajdonos_id;
+                    }
                 }
             }
+
+            
+        }
+
+        public static void SzamlaFeltolt(int tulajdonos_id, string szamlaszam, decimal egyenleg) {
+            string query = "INSERT INTO szamlak (tulajdonos_id,szamlaszam,egyenleg) VALUES (@tulajdonos_id,@szamlaszam,@egyenleg) ";
+            using (MySqlConnection conn = new MySqlConnection(connect)) {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query,conn)) {
+                    cmd.Parameters.AddWithValue("@tulajdonos_id", tulajdonos_id);
+                    cmd.Parameters.AddWithValue("@szamlaszam", szamlaszam);
+                    cmd.Parameters.AddWithValue("@egyenleg", egyenleg);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        
         }
     }
 }
