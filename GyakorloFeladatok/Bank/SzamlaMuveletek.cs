cb37cd4bd5e30b71b006862651d6d3ee516a0202
Reni98 <93825160+Reni98@users.noreply.github.com>
2025-03-20 10:31:46 +0100

    Add files via upload

---
+++ GyakorloFeladatok/Bank/SzamlaMuveletek.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class SzamlaMuveletek
+    {
+        public static string connect = "server=localhost;database = bank;user = root; password = ;";
+
+        public static void EgyenlegLekeres() {
+            string query = "SELECT szamlaszam,egyenleg FROM szamlak";
+            using (MySqlConnection conn = new MySqlConnection(connect)) {
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(query,conn)) {
+                    using (MySqlDataReader reader = cmd.ExecuteReader()) {
+                        while (reader.Read()) {
+                            string szamlaszam = reader.GetString("szamlaszam");
+                            decimal egyenleg = reader.GetDecimal("egyenleg");
+                        }
+
+                        Console.WriteLine("------Számla adatai-------");
+                        Console.WriteLine($"Szamlaszam: {reader["szamlaszam"]},\n Egyenleg: {reader["egyenleg"]}");
+
+                    }
+                
+                }
+            }
+        
+    }
+
+        public static void Befizetes(string szamlaszam,decimal osszeg) {
+            Console.WriteLine("Add meg a számlaszámod:");
+             szamlaszam = Console.ReadLine();
+            Console.WriteLine("Add meg az összeget:");
+             osszeg = decimal.Parse(Console.ReadLine());
+
+            using (MySqlConnection conn = new MySqlConnection(connect)) {
+                conn.Open();
+                string query = "UPDATE szamlak SET egyenleg = egyenleg + @osszeg WHERE szamlaszam = @szamlaszam";
+                using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+                    cmd.Parameters.AddWithValue("@szamlaszam", szamlaszam);
+                    cmd.Parameters.AddWithValue("@osszeg", osszeg);
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Sikeresen módosítva lett az egyenleg.");
+                }
+            
+            }
+
+        }
+}
+
+}
